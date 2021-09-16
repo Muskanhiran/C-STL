30a456ad2df98f2edf451c8b3789e1d19f20c18e
Muskan hiran <67425126+Muskanhiran@users.noreply.github.com>
2021-09-16 07:37:13 +0000

    Using array STL

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(){
+  
+  array<int, 4> ar = {1,2,3,4};
+  int size = ar.size();
+
+  cout<<"Size of an array is"<<size<<endl;
+  cout<<"Elements present in an array are:"<<endl;
+  for(int i = 0;i < size;i++){
+    cout<<ar[i]<<endl;
+  }
+  cout<<"Element at index 2 is "<<ar.at(2)<<endl;
+  return 0;
 }
